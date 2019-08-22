8b9978ab84c5b9fc89b143baad6df11ae98cd781
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-22 22:41:09 +0300

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//Напишите шуточную программу «Дешифратор», которая 
+//бы в текстовом файле могла бы заменить все предлоги на слово «ГАВ!».
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\";
+            Process.Start("explorer.exe", path);
+            string str = File.ReadAllText(path+"File.txt", Encoding.Default);
+            Console.WriteLine(str);
+            Console.WriteLine(new string('-', 20));
+            string newstr = Regex.Replace(str, @"\W(в)\W|\W(от)\W|\W(на)\W|\W(у)\W|\W(за)\W|\W(с)\W|\W(под)\W|\W(над)\W", @" ГАВ! ");
+            Console.WriteLine(newstr);
+            File.WriteAllText(path+"NewFile.txt", newstr, Encoding.Default);
+            Console.WriteLine("Для выхода нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+    }
+}
