be85249c54fed316f706a69bb1e6deca50f593e7
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-22 21:12:00 +0300

    create empty project string

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Напишите консольное приложение, позволяющие пользователю зарегистрироваться 
+//под «Логином», состоящем только из символов латинского алфавита, и пароля,
+//состоящего из цифр и символов.
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
